f2c045ecf2579ca7b25c5be7666f5d08b8b131c8
Khan <khanmujeebullah@gmail.com>
2026-05-04 13:31:32 +0500

    added startup folder in lecture07

---
+++ lecture07/startup/src/Welcome.jsx
@@ -0,0 +1,11 @@
+import './Welcome.css'
+const Welcome = () => {
+  return (
+    <div className='card'>
+        <h1>Rahul Sharma</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, autem.</p>
+    </div>
+  )
+}
+
+export default Welcome;
